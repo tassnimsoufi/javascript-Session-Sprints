ac20bd5e3184f9aae1ea34ba343b8ddeb807189c
tassnim soufi <tassnimsoufi@gmail.com>
2024-01-24 19:18:46 +0100

    this is my work logical operator

--- js/warmups/logicalOperator.js
+++ js/warmups/logicalOperator.js
@@ -1,2 +1,44 @@
 // This is tassnim 
 // this is nour 
+// FIRST EXERCICE
+function opposite(boolean){
+    return !boolean
+}
+// SECOND EXERCICE
+
+function ex1(boll){
+    return !boll
+
+}
+function ex2(){
+    console.log(true&&true)
+    console.log(true&&false)
+    console.log(false&&true)
+    console.log(false&&false)
+
+}
+function ex3(){
+    console.log(true||true)
+    console.log(true||false)
+    console.log(false||true)
+    console.log(false||false)
+
+}
+function ex4(){
+    console.log(3>4)
+    console.log(4>3)
+    console.log (!(4>3))
+    console.log(4<=4 && 5<6)
+    console.log (3<4 && 8<12)
+    console.log(3===3 || 4===4)
+    console.log()
+}
+function ex9(a){
+    if(a>5 && a<20){
+        return true
+    }
+     else{
+        return false
+        }
+    }
+
